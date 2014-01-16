efea1659c30fea4e6c5455806b90d9ae4d49dd18
David Khourshid <David.Khourshid@Assist-RX.com>
2014-01-16 00:49:52 -0500

    Initialization

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ARX BOX</title>
+</head>
+<body>
+    <header class="row">
+        
+    </header>
+    <section class="row">
+        <div class="large-3 columns">
+            sidebar
+        </div>
+        <div class="large-9 columns">
+            main area
+        </div>
+    </section>
+</body>
+</html>
