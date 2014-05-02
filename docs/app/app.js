ad611f7f0e92491b29c91166acf6476b5f00daf7
David Khourshid <davidkpiano@gmail.com>
2014-05-02 02:57:28 -0400

    Various theme commits (in progress)

--- docs/app/app.js
+++ docs/app/app.js
@@ -4,20 +4,21 @@ var app = angular.module('cadenza', ['ui.router', 'ngAnimate']);
 
 app.config(['$stateProvider', '$urlRouterProvider', 
     function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('grid', {
-                url: '/grid',
-                templateUrl: 'views/grid.html',
-                controller: 'ComponentController',
-                controllerAs: 'component'
-            })
+        var components = [
+            'grid',
+            'panels',
+            'themes'
+        ];
 
-            .state('panels', {
-                url: '/panels',
-                templateUrl: 'views/panels.html',
-                controller: 'ComponentController',
-                controllerAs: 'component'
-            });
+        _.each(components, function(component) {
+            $stateProvider
+                .state(component, {
+                    url: '/' + component,
+                    templateUrl: 'views/' + component + '.html',
+                    controller: 'ComponentController',
+                    controllerAs: 'component'
+                });
+        });
     }]);
 
 app.controller('ComponentController', [function() {
