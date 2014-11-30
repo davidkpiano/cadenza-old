d6474d2b20c4d79bcb8f78bfc557669ad328ceba
David Khourshid <davidkpiano@gmail.com>
2014-11-30 11:21:49 -0500

    Added component creation task to gulp file, separated out drawer component

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var livereload = require('gulp-livereload');
 var autoprefixer = require('gulp-autoprefixer');
 var markdown = require('gulp-markdown');
 var highlight = require('gulp-highlight');
+var prompt = require('gulp-prompt');
+
+// Non-gulp requires
+var fs = require('fs');
 
 gulp.task('build', ['scss']);
 
@@ -24,6 +28,31 @@ gulp.task('docs', function() {
         .pipe(markdown())
         .pipe(highlight())
         .pipe(gulp.dest('docs/build'));
+});
+
+gulp.task('component', function() {
+    gulp.src('app.js')
+        .pipe(prompt.prompt({
+            type: 'input',
+            name: 'component',
+            message: 'Name of component?'
+        }, function(res) {
+            var baseDir = 'scss/modules/' + res.component + '/';
+
+            fs.mkdir(baseDir, function(err) {
+                if (err) return console.log(err);
+
+                var moduleContent = "@import\n\t'_mixins',\n\t'_components',\n\t'_themes',\n\t'_exports';";
+
+                fs.writeFileSync(baseDir + '_module.scss', moduleContent);
+                fs.writeFileSync(baseDir + '_components.scss', '');
+                fs.writeFileSync(baseDir + '_mixins.scss', '');
+                fs.writeFileSync(baseDir + '_themes.scss', '');
+                fs.writeFileSync(baseDir + '_exports.scss', '');
+
+                console.log('Component ' + res.component + ' created.');
+            });
+        }));
 });
 
 // Default Task
